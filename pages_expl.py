cbeb62ad6ccdaa604cbacc1f30b2e6914c618cc3
root <root@220196U-B117M03.lise.kreizenn.bzh>
2024-04-03 08:37:11 +0200

    added bug managment

--- pages_expl.py
+++ pages_expl.py
@@ -12,6 +12,9 @@ def verify_url(url:str) -> str:
     This function take as argument an url to test as str, and return a message to know if the url
     is correct or not.
     """
+    if type(url) != str:
+        return ""
+
     try:
         test = requests.get(url) # we try to make a request
         if test.status_code == 404: 
@@ -23,22 +26,29 @@ def verify_url(url:str) -> str:
 
 
 class explorer:
-    def __init__(self,site_url:str):
+    def __init__(self,site_url:str) -> None:
         """
         This class is used as the explorer itself.
         """
+        if type(site_url) != str:
+            return None
+
         self.site_url = site_url # this is the url provided by user
         self.url_list = [self.site_url] # this is the list used to store the nexts links to process
         self.urls_and_sources = {} # this dictionnary store the "final data"
 
 
-    def find_internal_links(self,url:str):
+    def find_internal_links(self,url:str) -> tuple:
         """
         This method take as argument the url of a web page and return the url itself and all internal links found in the html content of the page.
         """
+        if type(url) != str:
+            return []
+
         internal_links = [] # list of internal links
         domain_name = urlparse(url).netloc # domain name of the site (this is useful to know if a link is internal or external)
         soup = bs(requests.get(url).content, "html.parser") # find the html content
+
         for a in soup.findAll("a"):
             """
             for each link we find :
@@ -57,10 +67,10 @@ class explorer:
             if domain_name in href:
                 internal_links += [href] # if the link is internal, we add it to the list
 
-        return url,internal_links #return of the list
+        return (url,internal_links) #return of the url and the list
 
 
-    def explore(self):
+    def explore(self) -> None:
         """
         this method the the "main" method of the class explorer.
         """
@@ -76,10 +86,13 @@ class explorer:
                     self.urls_and_sources[base_url] = [link] 
     
     
-    def save_results(self,filename):
+    def save_results(self,filename:str) -> None:
         """
         This method is used to store data into a file if asked by user
         """
+        if type(filename) != str:
+            return None
+
         with open(filename,"w") as file:
             for key,value in self.urls_and_sources.items():
                 file.write(key + " : \n") # we write the source url
@@ -89,7 +102,7 @@ class explorer:
             file.close()
     
     
-def main():
+def main() -> None:
     """
     This is the main function. After doing some tests about the url provided by the user,
     we create an explorer and we use it. Then we verify if the user specified a file to store results.
@@ -100,13 +113,13 @@ def main():
         url = argv[1] #try to store url provided by user 
     except IndexError:
         print("[-] error : url not specified")
-        return
+        return None
 
     test_url = verify_url(url) # verification of the url
 
     if test_url == "error":
         print(f"[-] error : url {url} is invalid")
-        return
+        return None
 
     pages_explorer = explorer(url) # create the explorer
     pages_explorer.explore() # explore
@@ -115,7 +128,7 @@ def main():
         filename = argv[2] # try to find the storing file name
         print(f"Saving into {filename}...")
     except IndexError:
-        return
+        return None
 
     pages_explorer.save_results(filename) # store urls into the file
     
